98a2605fab4a2928a5e2d35fae048ac353b8bc20
dev-deanie <dev.deanie@gmail.com>
2022-07-27 10:05:39 +0900

    first commit for todo list with vue

---
+++ src/components/TodoFooter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="clearAllContainer">
+      <span class="claerAllBtn" v-on:click="clearTodo">
+          Clear All
+      </span>
+  </div>
+</template>
+
+<script>
+export default {
+    methods : {
+        clearTodo : function(){
+            localStorage.clear();
+        }
+    }
+}
+</script>
+
+<style scoped>
+.clearAllContainer{
+    width:8.5rem;
+    height: 50px;
+    line-height: 50px;
+    background-color: white;
+    border-radius: 5px;
+    margin: 0 auto
+}
+.claerAllBtn{
+    color: #e20303;
+    display: block;;
+}
+</style>
